b74346cafda6e8b348d995ddc52328a1dc16deff
KoHsetGyi <mr.kosetgyi@gmail.com>
2019-05-09 15:51:24 +0630

    make auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,8 @@ class CreateUsersTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   
+        Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');

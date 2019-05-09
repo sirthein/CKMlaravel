bf707a8eee13b24092d064e485a04bb105c76728
KoHsetGyi <mr.kosetgyi@gmail.com>
2019-05-09 16:33:35 +0630

    change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CKM LARAVEL PROJECT
                 </div>
 
                 <div class="links">
